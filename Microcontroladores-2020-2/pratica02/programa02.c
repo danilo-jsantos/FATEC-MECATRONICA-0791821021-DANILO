1a5895caeb478c9710933d2e71a28c0940a86462
danilo-jsantos <69904679+danilo-jsantos@users.noreply.github.com>
2020-09-02 09:32:11 -0300

    Create programa02.c

---
+++ Microcontroladores-2020-2/pratica02/programa02.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+ float A,B,X;
+ printf("informe os valores de B e A\n");
+ scanf("%f%f", &B, &A);
+ X = -B/A;
+ printf("o valor de X eh: %f", X);
+  return 0;
+}
