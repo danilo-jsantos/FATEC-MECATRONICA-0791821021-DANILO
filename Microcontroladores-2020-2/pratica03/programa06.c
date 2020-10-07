183c74e904223e5334560550f1c4652960ba20c0
danilo-jsantos <69904679+danilo-jsantos@users.noreply.github.com>
2020-10-06 22:02:05 -0300

    Create programa06.c

---
+++ Microcontroladores-2020-2/pratica03/programa06.c
@@ -0,0 +1,14 @@
+# inclui  < stdio.h >
+
+int  main ( void ) {
+  int temperaturas [ 3 ];
+  // Ler as 3 temperaturas
+  int i;
+  para (i = 0 ; i < 3 ; i ++) {
+    printf ( " Informe a temperatura % i : " , i);
+    scanf ( " % i " , & temperatura [i]);
+  }
+
+
+  return  0 ;
+}
